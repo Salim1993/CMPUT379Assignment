b4ccda8414347cb2b3f2de54ee7848d76b6f21f8
Salim1993 <benkhale@ualberta.ca>
2015-11-09 18:08:50 -0700

    complete pipes i believe

--- test/bufferTest.c
+++ test/bufferTest.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
 /* This program forks and and the prints whether the process is
  *   - the child (the return value of fork() is 0), or
@@ -23,9 +24,16 @@
 	int wr[2];		//child will write
 };
 
+struct process
+{
+   int  processID;
+   char  name[256];
+   int   time;
+};
+
 struct child{
 	int childPID;
-	int status = 1;
+	int status;
 	//might need address to pipe;
 	struct process proc;
 };
@@ -39,13 +47,6 @@ struct pipeMessage{
 	struct process procMessage;
 };
 
-struct process
-{
-   int  processID;
-   char  name[256];
-   int   time;
-};
-
 
 int main( void ) {
 
@@ -56,25 +57,38 @@ int main( void ) {
 	int counter = 3;
 	int	fd[256][2];
 	char line[256];
+	char readline[256];
 
 	struct Pipes pipeList[256];
 
 	for(i=0;i < counter; i++){
-		pipe(pipeList[i].rdwr);
-		//pipe(fd[i]);
+		pipe(pipeList[i].rd);
+		pipe2(pipeList[i].wr,O_NONBLOCK);
+		//pipe(fd[i]);		
+		int writestatus = write(pipeList[i].rd[1], "lolol",6);
+		
+
 		int pid = fork();
 		if(pid < 0){
 	    	//err_sys("fork error");
 	    }
 		else if ( pid == 0 ) {  //child
+
+			
+			close(pipeList[i].rd[1]);
+			int k = read(pipeList[i].rd[0], readline, 256);
+			printf("(%d) THe child has recieved the line from parent: %s\n",k,readline);
+			close(pipeList[i].rd);
+			
+
 			char *tempHolder = malloc(6);
 			sprintf(tempHolder, "%d", getpid());
-			close(pipeList[i].rdwr[0]);
-			int status = write(pipeList[i].rdwr[1], tempHolder, 6);
+			close(pipeList[i].wr[0]);
+			int readstatus = write(pipeList[i].wr[1], tempHolder, 6);
 			//close(fd[i][0]);
 			//int status = write(fd[i][1], tempHolder, 6);
 
-			printf("The child %d has attempted write (%d). \n",getpid(),status);
+			printf("The child %d has attempted write (%d). \n",getpid(),readstatus);
 			free(tempHolder);
 			exit(EXIT_SUCCESS);
 		} else {  //parent
@@ -82,14 +96,19 @@ int main( void ) {
 			printf("this is the iteration %d and the pid is %d\n",i,pid );
 		}
 	}
-	int j;
-	for(j = 0; j < counter; j++){
-		//close(fd[j][1]);
-		close(pipeList[j].rdwr[1]);
-		//int n = read(fd[j][0], line, 256);
-		int n = read(pipeList[j].rdwr[0], line, 256);
-		int propPID = atoi(line);
-		printf("(%d)   child returned its pid which is %s\n",n, line);
+	int j = 0;
+	int n;
+	while(j < counter){
+		n = read(pipeList[j].wr[0], line, 256);
+		if (n > 1){
+			//close(fd[j][1]);
+			close(pipeList[j].wr[1]);
+			//int n = read(fd[j][0], line, 256);
+			int propPID = atoi(line);
+			printf("(%d)   child returned its pid which is %s\n",n, line);
+			close(pipeList[j].wr);
+			j++;
+		}
 	}
 	return 0;
 }
